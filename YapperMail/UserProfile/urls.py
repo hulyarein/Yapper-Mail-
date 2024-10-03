c6c0345e46395abf9e3b4c3ece00f068bb238d89
Ninin <ninocabiltes@gmail.com>
2024-10-03 11:00:49 +0800

    Finished Templates needs housekeeping

--- YapperMail/UserProfile/urls.py
+++ YapperMail/UserProfile/urls.py
@@ -3,5 +3,12 @@ from . import views
 
 urlpatterns = [
     path('', views.profilePage,name='profile'),
-    path('profilePage/', views.profilePage, name='profile')
+    path('profilePage/', views.profilePage, name='profile'),
+    path('profilePage/PersonalInfo/Name/', views.changeName, name='changeName'),
+    path('profilePage/PersonalInfo/Birthday', views.changeBday, name='changeBday'),
+    path('profilePage/PersonalInfo/Gender', views.changeGender, name='changeGender'),
+    path('profilePage/ContactInfo/Number', views.changeNumber, name='changeNumber'),
+    path('profilePage/ContactInfo/EmailAddress', views.changeEmail, name='changeEmail'),
+    path('profilePage/Addresses/Home', views.changeHome, name='changeHome'),
+    path('profilePage/Addresses/Work', views.changeWork, name='changeWork'),
 ]
--- YapperMail/UserProfile/views.py
+++ YapperMail/UserProfile/views.py
@@ -1,5 +1,65 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
 
 # Create your views here.
 def profilePage(request):
-    return render(request,"profilePage.html")
+    session_check = checkSession(request)
+    if isinstance(session_check, HttpResponse):
+        return session_check
+
+    return render(request, "profilePage.html")
+
+def changeName(request):
+    session_check = checkSession(request)
+    if isinstance(session_check, HttpResponse):
+        return session_check
+
+    return render(request,"nameChange.html")
+
+def changeBday(request):
+    session_check = checkSession(request)
+    if isinstance(session_check, HttpResponse):
+        return session_check
+
+    return render(request, "bdayChange.html")
+
+def changeGender(request):
+    session_check = checkSession(request)
+    if isinstance(session_check, HttpResponse):
+        return session_check
+
+    return render(request, "genderChange.html")
+
+def changeNumber(request):
+    session_check = checkSession(request)
+    if isinstance(session_check, HttpResponse):
+        return session_check
+
+    return render(request, "numberChange.html")
+
+def changeEmail(request):
+    session_check = checkSession(request)
+    if isinstance(session_check, HttpResponse):
+        return session_check
+
+    return render(request, "emailChange.html")
+
+def changeWork(request):
+    session_check = checkSession(request)
+    if isinstance(session_check, HttpResponse):
+        return session_check
+
+    return render(request, "workChange.html")
+
+def changeHome(request):
+    session_check = checkSession(request)
+    if isinstance(session_check, HttpResponse):
+        return session_check
+
+    return render(request, "homeChange.html")
+
+def checkSession(request):
+    print("hi")
+    # if not request.user.is_authenticated:
+    #     return redirect('changeBday')
+    # return None
