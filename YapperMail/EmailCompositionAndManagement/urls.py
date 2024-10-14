4c9a7d904db0deb7068c5509edd1a356d05d4642
zyle-geralde <zylgerdlp19@gmail.com>
2024-10-14 21:38:51 +0800

    email edit complete

--- YapperMail/EmailCompositionAndManagement/urls.py
+++ YapperMail/EmailCompositionAndManagement/urls.py
@@ -6,7 +6,8 @@ urlpatterns = [
     path('composeEmail/',views.email_composition,name='email_composition'),
     path('emailSentView/',views.email_sent_view,name='emailSentView'),
     path('emailReceiveView/',views.email_reply_view,name='emailReceiveView'),
-    path('editEmail/',views.edit_email,name='editEmail'),
+    path('editEmail/<int:pk>',views.edit_email,name='editEmail'),
     path('editReply/',views.edit_reply,name='editReply'),
-    path('download/<path:filename>/', views.download_file, name='download_file')
+    path('download/<path:filename>/', views.download_file, name='download_file'),
+    path('changeImage/<int:pk>',views.editExistingImage,name = "editExistingImage"),
 ]
--- YapperMail/EmailCompositionAndManagement/views.py
+++ YapperMail/EmailCompositionAndManagement/views.py
@@ -6,6 +6,9 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.http import FileResponse, Http404
 import os
 from urllib.parse import unquote
+import json
+from django.http import JsonResponse,HttpResponse
+
 
 # Create your views here.
 
@@ -66,7 +69,7 @@ def email_sent_view(request):
         form = ReplyComposeForm()
 
     
-    getEmail = Email.objects.get(id = 6)
+    getEmail = Email.objects.get(id = 7)
 
     getFiles = EmailFiles.objects.filter(emailId = getEmail)
         
@@ -84,17 +87,87 @@ def email_reply_view(request):
         
     return render(request,"emailReceiveView.html",{'form':form})
 
-def edit_email(request):
+def edit_email(request,pk):
+    getEmail = Email.objects.get(id = pk)
+    getFiles = EmailFiles.objects.filter(emailId = getEmail)
+    
     if request.method == "POST":
         form = EditEmailForm(request.POST)
         if form.is_valid():
             toUser = form.cleaned_data['toUser']
             subject = form.cleaned_data['subject']
+            description = form.cleaned_data['description']
+
+            try:
+                getEmail.subject = subject
+                getEmail.content = description
+
+                getEmail.save()
+
+                uploaded_files = request.FILES.getlist('file')
+                if uploaded_files:
+                    for uploaded_file in uploaded_files:
+                        email_file = EmailFiles(
+                            fromUser=getEmail.fromUser,
+                            toUser=getEmail.toUser,
+                            emailId=getEmail, 
+                            file=uploaded_file
+                        )
+                        email_file.save()  
+                else:
+                    print("No files were uploaded.")
+
+                return redirect(f'../emailSentView')
+            except ObjectDoesNotExist:
+                error_message = True
+        else:
+            print("not valid")
+
+
+
 
     else:
-        form = EditEmailForm()
+        initialValue = {
+            'toUser':getEmail.toUser,
+            'subject':getEmail.subject,
+            'description':getEmail.content,
+        }
+        form = EditEmailForm(initial = initialValue)
 
-    return render(request,"editEmail.html",{'form':form})
+    return render(request,"editEmail.html",{'form':form,'emailSent':getEmail,'emailFilesSent':getFiles})
+
+def editExistingImage(request,pk):
+    '''getEmail = Email.objects.get(id = pk)
+    getFiles = EmailFiles.objects.filter(emailId = getEmail)
+
+    print(getFiles)
+    return JsonResponse({"ok":"OK"})'''
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)  # Parse JSON data
+            list_to_delete = data.get('listme', [])  # Get list of filenames to delete
+            print(list_to_delete)
+
+            # Get the email instance by primary key (pk)
+            email_instance = get_object_or_404(Email, id=pk)
+
+            # Query files linked to the email instance and filter by filenames
+            files_to_delete = EmailFiles.objects.filter(emailId=email_instance).exclude(file__in=list_to_delete)
+            deleted_files_count = files_to_delete.delete()
+
+            response = {
+                'message': 'Files deleted successfully',
+                'deleted_files_count': deleted_files_count[0]  # Number of deleted files
+            }
+            return JsonResponse(response, status=200)
+
+        except json.JSONDecodeError as e:
+            return JsonResponse({'error': f'Invalid JSON: {str(e)}'}, status=400)
+
+    return JsonResponse({'error': 'Invalid request method'}, status=405)
+
+
+
 
 def edit_reply(request):
     if request.method == "POST":
