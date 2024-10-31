4eba123b3ee2edb002a1f1f8de3e48a73510a420
zyle-geralde <zylgerdlp19@gmail.com>
2024-11-01 07:20:00 +0800

    added model to Team Emails

--- YapperMail/TeamEmails/views.py
+++ YapperMail/TeamEmails/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404,redirect
+from .forms import EmailComposeForm,ReplyComposeForm,EditEmailForm,EditReplyForm,SearchForm
+from .models import Email,EmailFiles,TemporaryUser,Reply,ReplyFiles
+from django.contrib.auth.models import User
+from django.core.files.storage import FileSystemStorage
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import FileResponse, Http404
+import os
+from urllib.parse import unquote
+import json
+from django.http import JsonResponse,HttpResponse
+from django.db.models import Q
 
-# Create your views here.
