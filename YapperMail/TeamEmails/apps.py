0d8ad5ef8617911fe5df7a1f950c875940663d8d
zyle-geralde <zylgerdlp19@gmail.com>
2024-11-01 06:47:45 +0800

    added Team Emails app

---
+++ YapperMail/TeamEmails/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TeamemailsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'TeamEmails'
--- YapperMail/YapperMail/settings.py
+++ YapperMail/YapperMail/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'EmailCompositionAndManagement',
     'compressor',
     'phonenumber_field',
+    'TeamEmails',
 ]
 
 MIDDLEWARE = [
